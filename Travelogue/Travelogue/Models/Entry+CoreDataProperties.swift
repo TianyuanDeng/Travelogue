7aff9f4a4be1554806a77a8cdfb3bf8568584d0b
Tianyuan Deng <wergelesn@missouri.edu>
2019-05-10 12:44:19 -0500

    Set CoreData Relationships

---
+++ Travelogue/Travelogue/Models/Entry+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  Entry+CoreDataProperties.swift
+//  Travelogue
+//
+//  Created by Deng tianyuan on 5/10/19.
+//  Copyright © 2019 Deng tianyuan. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Entry {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Entry> {
+        return NSFetchRequest<Entry>(entityName: "Entry")
+    }
+
+    @NSManaged public var content: String?
+    @NSManaged public var rawDate: NSDate?
+    @NSManaged public var rawImage: NSData?
+    @NSManaged public var title: String?
+    @NSManaged public var tripBlog: Trip?
+
+}
---
+++ Travelogue/Travelogue/Models/Trip+CoreDataClass.swift
@@ -0,0 +1,31 @@
+//
+//  Trip+CoreDataClass.swift
+//  Travelogue
+//
+//  Created by Deng tianyuan on 5/10/19.
+//  Copyright © 2019 Deng tianyuan. All rights reserved.
+//
+//
+
+import UIKit
+import CoreData
+
+@objc(Trip)
+public class Trip: NSManagedObject {
+    var entries: [Entry]? {
+        return self.rawEntries?.array as? [Entry]
+    }
+    
+    convenience init?(title: String?, contents: String?) {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else {
+            return nil
+        }
+        
+        self.init(entity: Trip.entity(), insertInto: managedContext)
+        
+        self.title = title
+        self.content = contents
+    }
+}
